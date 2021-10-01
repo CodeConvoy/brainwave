13e31fb8eb33c60ccd04c9e387ce98b654f091da
Cooper Saye <coopersaye@gmail.com>
2021-10-01 12:57:02 -0700

    Clean up files.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+const title = "Brainwave";
+const description = "Rich and centralized idea development.";
+
+export default function App(props) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
