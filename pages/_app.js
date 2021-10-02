f1844b187ebb69239b89a442669a25db3bdb69a2
Cooper Saye <coopersaye@gmail.com>
2021-10-01 22:44:39 -0700

    Fix firebase initialization.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import Head from 'next/head';
 
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { firebaseConfig } from '../firebaseConfig';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 import '../styles/globals.css';
 
 const title = "Brainwave";
 const description = "Rich and centralized idea development.";
 
+// initialize firebase
+if (!getApps().length) initializeApp(firebaseConfig);
+
 export default function App(props) {
+  useAuthState(getAuth());
+
   const { Component, pageProps } = props;
 
   return (
