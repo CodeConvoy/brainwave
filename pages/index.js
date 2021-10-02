04134cb4b12cf865cade746fb1fc0ffc6f97ebe3
Cooper Saye <coopersaye@gmail.com>
2021-10-01 23:20:07 -0700

    Create homepage.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
+import Image from 'next/image';
+
+import { getAuth, signOut } from 'firebase/auth';
+import signInWithGoogle from '../util/signInWithGoogle';
 
 import styles from '../styles/Index.module.css';
 
 export default function Index() {
+  const auth = getAuth();
+
   return (
     <div>
+      <h1><Image src="/logo.png" width="48" height="48" /> BRAINWAVE</h1>
+      {
+        auth.currentUser ?
+        <button onClick={() => signOut(auth)}>
+          Sign Out
+        </button> :
+        <button onClick={signInWithGoogle}>
+          Sign in with Google
+        </button>
+      }
     </div>
   );
 }
