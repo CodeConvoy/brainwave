5b46181ec1154a4bbd36b446c21d1aa12e75dbd4
Cooper Saye <coopersaye@gmail.com>
2021-10-01 23:21:51 -0700

    Improve styling.

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,7 @@ export default function App(props) {
         <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;900&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </>
