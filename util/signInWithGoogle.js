d283808a6ba0a97c9ced9344188d30e2503574fa
Cooper Saye <coopersaye@gmail.com>
2021-10-01 22:42:38 -0700

    Create signInWithGoogle function.

---
+++ util/signInWithGoogle.js
@@ -0,0 +1,8 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+// opens google sign in popup
+export default async function signInWithGoogle() {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  await signInWithPopup(auth, provider);
+}
