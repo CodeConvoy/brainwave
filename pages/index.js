544f7012c93d4d5dd32f11d5a7bb61ee1a94c10a
Cooper Saye <coopersaye@gmail.com>
2021-10-01 22:29:33 -0700

    Initialize firebase.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
+import { initializeApp, getApps } from 'firebase/app';
+import { firebaseConfig } from '../firebaseConfig';
+
 import styles from '../styles/Index.module.css';
+
+// initialize firebase
+if (!getApps().length) initializeApp(firebaseConfig);
 
 export default function Index() {
   return (
