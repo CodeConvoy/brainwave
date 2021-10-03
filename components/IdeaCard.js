e075905c5da06a51df28e5d1e23ebca5043f8f41
Cooper Saye <coopersaye@gmail.com>
2021-10-02 22:18:18 -0700

    Link to idea page on card.

--- components/IdeaCard.js
+++ components/IdeaCard.js
@@ -1,16 +1,21 @@
+import Link from 'next/link';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 
 import styles from '../styles/components/IdeaCard.module.css';
 
 export default function IdeaCard(props) {
-  const { title } = props;
+  const { title, id } = props;
 
   return (
-    <Card className={styles.container}>
-      <CardContent>
-        <p>{title}</p>
-      </CardContent>
-    </Card>
+    <Link href={`/ideas/${id}`}>
+      <a className={styles.container}>
+        <Card>
+          <CardContent>
+            <p>{title}</p>
+          </CardContent>
+        </Card>
+      </a>
+    </Link>
   );
 }
