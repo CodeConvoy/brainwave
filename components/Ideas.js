9bd8b2c636d7bb7a45f9bea96a4639894ef8aeb8
Cooper Saye <coopersaye@gmail.com>
2021-10-02 21:49:30 -0700

    Extract Ideas component.

---
+++ components/Ideas.js
@@ -0,0 +1,40 @@
+import IdeaCard from '../components/IdeaCard';
+
+import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+
+import styles from '../styles/components/Ideas.module.css';
+
+export default function Ideas() {
+  const auth = getAuth();
+  const db = getFirestore();
+
+  const [ideas, setIdeas] = useState(undefined);
+
+  // get ideas query
+  const uid = auth.currentUser.uid;
+  const ideasRef = collection(db, 'ideas');
+  const ideasQuery = query(ideasRef, where('creator', '==', uid));
+
+  // retrieves ideas from firebase
+  async function getIdeas() {
+    const ideasDocs = (await getDocs(ideasQuery)).docs;
+    setIdeas(ideasDocs.map(ideaDoc => ({ ...ideaDoc.data(), id: ideaDoc.id })));
+  }
+
+  // get ideas on start
+  useEffect(() => {
+    getIdeas();
+  }, [uid]);
+
+  return (
+    <div>
+      {
+        ideas.map((idea, i) =>
+          <IdeaCard {...idea} key={i} />
+        )
+      }
+    </div>
+  );
+}
--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,8 @@
-import IdeaCard from '../components/IdeaCard';
+import Ideas from '../components/Ideas';
 
 import { useEffect, useState } from 'react';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, collection, query, where, getDocs, addDoc } from 'firebase/firestore';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -10,40 +10,20 @@ export default function Home() {
   const auth = getAuth();
   const db = getFirestore();
 
-  const [ideas, setIdeas] = useState(undefined);
   const [title, setTitle] = useState('');
 
   // get ideas query
   const uid = auth.currentUser?.uid;
   const ideasRef = collection(db, 'ideas');
-  const ideasQuery = query(ideasRef, where('creator', '==', uid ?? 'null'));
 
   // creates a new idea
   async function createIdea() {
     await addDoc(ideasRef, { title, creator: uid });
   }
 
-  // retrieves ideas from firebase
-  async function getIdeas() {
-    const ideasDocs = (await getDocs(ideasQuery)).docs;
-    setIdeas(ideasDocs.map(ideaDoc => ({ ...ideaDoc.data(), id: ideaDoc.id })));
-  }
-
-  // get ideas on start
-  useEffect(() => {
-    getIdeas();
-  }, [uid]);
-
   return (
     <div>
-      <div>
-        {
-          ideas &&
-          ideas.map((idea, i) =>
-            <IdeaCard {...idea} key={i} />
-          )
-        }
-      </div>
+      <Ideas />
       <form onSubmit={e => {
         e.preventDefault();
         createIdea();
