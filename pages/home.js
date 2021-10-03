d08aecf779f0f9e9763e2f7c7573c9a14f9b54f5
Cooper Saye <coopersaye@gmail.com>
2021-10-02 21:37:21 -0700

    Implement idea retrieval.

--- pages/home.js
+++ pages/home.js
@@ -1,15 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, query, where, getDocs, addDoc } from 'firebase/firestore';
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
+  const auth = getAuth();
+  const db = getFirestore();
+
+  const [ideas, setIdeas] = useState(undefined);
   const [title, setTitle] = useState('');
 
+  // get ideas query
+  const uid = auth.currentUser?.uid;
   const ideasRef = collection(db, 'ideas');
+  const ideasQuery = query(ideasRef, where('creator', '==', uid ?? 'null'));
 
   // creates a new idea
   async function createIdea() {
     await addDoc(ideasRef, { title, creator: uid });
+  }
+
+  // retrieves ideas from firebase
+  async function getIdeas() {
+    const ideasDocs = (await getDocs(ideasQuery)).docs;
+    setIdeas(ideasDocs.map(ideaDoc => ({ ...ideaDoc.data(), id: ideaDoc.id })));
   }
 
   return (
