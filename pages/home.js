25fa99a890412f2e2e55f8aaa901c60f8c6fd031
Cooper Saye <coopersaye@gmail.com>
2021-10-02 21:30:24 -0700

    Implement idea creation.

--- pages/home.js
+++ pages/home.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
+  const [title, setTitle] = useState('');
+
+  const ideasRef = collection(db, 'ideas');
+
+  // creates a new idea
+  async function createIdea() {
+    await addDoc(ideasRef, { title, creator: uid });
+  }
+
   return (
     <div>
       <div>
       </div>
-      <form>
-        <input />
+      <form onSubmit={e => {
+        e.preventDefault();
+        createIdea();
+      }}>
+        <input
+          placeholder="title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          required
+        />
         <button>Create Idea</button>
       </form>
     </div>
