0731e62bc7296d577962590868eded743d99be87
Cooper Saye <coopersaye@gmail.com>
2021-10-02 22:17:39 -0700

    Retrieve idea.

--- pages/ideas/[id].js
+++ pages/ideas/[id].js
@@ -1,10 +1,29 @@
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
 import styles from '../../pages/Idea.module.css';
 
 export default function Idea() {
+  const db = getFirestore();
+  const router = useRouter();
+
   // get idea id
   const { id } = router.query;
 
   const [idea, setIdea] = useState(undefined);
+
+  // retrieves idea from firebase
+  async function getIdea() {
+    const ideaRef = doc(db, 'ideas', id);
+    const ideaDoc = await getDoc(ideaRef);
+    setIdea({ ...ideaDoc.data(), id: ideaDoc.id });
+  }
+
+  // get idea on start
+  useEffect(() => {
+    if (id) getIdea();
+  }, [id]);
 
   return (
     <div>
