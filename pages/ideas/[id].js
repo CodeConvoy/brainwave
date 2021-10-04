7a9c370bf229d791995fc498e0b2e32e2de25be2
Cooper Saye <coopersaye@gmail.com>
2021-10-03 22:08:13 -0700

    Implement free container scrolling.

--- pages/ideas/[id].js
+++ pages/ideas/[id].js
@@ -11,19 +11,21 @@ import { useRouter } from 'next/router';
 
 import styles from '../../styles/pages/Idea.module.css';
 
-const canvasSize = 512;
+const canvasWidth = 3000;
+const canvasHeight = 3000;
 
 let canvas, ctx;
+let container;
 
 let sketching = false;
 
 let prevX, prevY;
 let currX, currY;
-let offsetX, offsetY;
 
 const scrollSpeed = 4;
 
 export default function Idea() {
+  const containerRef = useRef();
   const canvasRef = useRef();
   const db = getFirestore();
   const router = useRouter();
@@ -45,17 +47,27 @@ export default function Idea() {
     if (id) getIdea();
   }, [id]);
 
+  // scrolls container by given values
+  function scrollContainer(x, y) {
+    if (x) container.scrollLeft += x;
+    if (y) container.scrollTop += y;
+  }
+
+  // called when canvas scrolled
+  function onWheel(e) {
+    e.preventDefault();
+    scrollContainer(e.deltaX, e.deltaY);
   }
 
   // on start
   useEffect(() => {
-    // get canvas context
+    // get ref contexts
     canvas = canvasRef.current;
     ctx = canvas.getContext('2d');
-    // set up key listener
-    window.addEventListener('keydown', onKeyDown);
-    // clean up listeners on return
-    return () => window.removeEventListener('keydown', onKeyDown);
+    container = containerRef.current;
+    // set up container wheel listener
+    container.addEventListener('wheel', onWheel);
+    return () => container.removeEventListener('wheel', onWheel);
   }, []);
 
   // gets previous and current mouse positions
