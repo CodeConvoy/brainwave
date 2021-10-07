762e0b604fae380da8f510d607d5b445d0283f6b
Cooper Saye <coopersaye@gmail.com>
2021-10-06 23:57:37 -0700

    Implement note movement.

--- components/Note.js
+++ components/Note.js
@@ -6,11 +6,25 @@ let dragging = false;
 
 let offsetX, offsetY;
 
+const holdOffset = 20;
+
 export default function Note(props) {
   const { index, removeNote, container } = props;
 
+  // moves note with given mouse data
+  function move(e) {
+    // get mouse position
+    const x = e.clientX + container.scrollLeft;
+    const y = e.clientY + container.scrollTop;
+    // set target position
+    const target = e.currentTarget;
+    target.style.left = `${x - holdOffset}px`;
+    target.style.top = `${y - holdOffset}px`;
+  }
+
   return (
     <div
+      className={styles.container}
       onMouseDown={e => { dragging = true; }}
       onMouseMove={e => { if (dragging) move(e); }}
       onMouseUp={e => { dragging = false; }}
