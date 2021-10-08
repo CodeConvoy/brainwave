50d7c3397dfa38111926700a6184a71ff001d34e
Cooper Saye <coopersaye@gmail.com>
2021-10-07 21:59:17 -0700

    Implement initial note position.

--- components/Note.js
+++ components/Note.js
@@ -1,5 +1,7 @@
 import CloseIcon from '@mui/icons-material/Close';
 
+import { useEffect, useRef } from 'react';
+
 import styles from '../styles/components/Note.module.css';
 
 let dragging = false;
@@ -7,9 +9,12 @@ let dragging = false;
 let offsetX, offsetY;
 
 const holdOffset = 20;
+const createOffset = 200;
 
 export default function Note(props) {
   const { index, removeNote, container } = props;
+
+  const noteRef = useRef();
 
   // moves note with given mouse data
   function move(e) {
@@ -22,8 +27,16 @@ export default function Note(props) {
     target.style.top = `${y - holdOffset}px`;
   }
 
+  // set note position on start
+  useEffect(() => {
+    const note = noteRef.current;
+    note.style.left = `${createOffset + container.scrollLeft}px`;
+    note.style.top = `${createOffset + container.scrollTop}px`;
+  }, []);
+
   return (
     <div
+      ref={noteRef}
       className={styles.container}
       onMouseDown={e => { dragging = true; }}
       onMouseMove={e => { if (dragging) move(e); }}
