d8ff7dcd199384477a4d5facc5f07166fca4ba19
Cooper Saye <coopersaye@gmail.com>
2021-10-07 22:04:04 -0700

    Implement note hold offset.

--- components/Note.js
+++ components/Note.js
@@ -11,10 +11,22 @@ let offsetX, offsetY;
 const holdOffset = 20;
 const createOffset = 200;
 
+let note;
+
 export default function Note(props) {
   const { index, removeNote, container } = props;
 
   const noteRef = useRef();
+
+  // sets note hold offset
+  function setOffset(e) {
+    const x = e.clientX + container.scrollLeft;
+    const y = e.clientY + container.scrollTop;
+    const noteX = parseInt(note.style.left);
+    const noteY = parseInt(note.style.top);
+    offsetX = x - noteX;
+    offsetY = y - noteY;
+  }
 
   // moves note with given mouse data
   function move(e) {
@@ -23,13 +35,13 @@ export default function Note(props) {
     const y = e.clientY + container.scrollTop;
     // set target position
     const target = e.currentTarget;
-    target.style.left = `${x - holdOffset}px`;
-    target.style.top = `${y - holdOffset}px`;
+    target.style.left = `${x - offsetX}px`;
+    target.style.top = `${y - offsetY}px`;
   }
 
   // set note position on start
   useEffect(() => {
-    const note = noteRef.current;
+    note = noteRef.current;
     note.style.left = `${createOffset + container.scrollLeft}px`;
     note.style.top = `${createOffset + container.scrollTop}px`;
   }, []);
@@ -38,7 +50,7 @@ export default function Note(props) {
     <div
       ref={noteRef}
       className={styles.container}
-      onMouseDown={e => { dragging = true; }}
+      onMouseDown={e => { dragging = true; setOffset(e); }}
       onMouseMove={e => { if (dragging) move(e); }}
       onMouseUp={e => { dragging = false; }}
       onMouseLeave={e => { dragging = false; }}
