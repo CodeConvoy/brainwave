ba46b27ab8f35d15cdc54c40fce2d6cf3b13def3
Cooper Saye <coopersaye@gmail.com>
2021-10-16 12:50:35 -0700

    Add header auth buttons.

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,15 @@
 import Image from 'next/image';
 
+import { getAuth, signOut } from 'firebase/auth';
+import signInWithGoogle from '../util/signInWithGoogle';
+
 import styles from '../styles/components/Header.module.css';
 
-export default function Header() {
+export default function Header(props) {
+  const { userData } = props;
+
+  const auth = getAuth();
+
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
@@ -14,6 +21,20 @@ export default function Header() {
         />
       </div>
       <h1>brainwave</h1>
+      <span className="flexfill" />
+      {
+        userData &&
+        <p>Signed in as @{userData.username}</p>
+      }
+      {
+        auth.currentUser ?
+        <button className="outlinebtn" onClick={() => signOut(auth)}>
+          Sign Out
+        </button> :
+        <button className="outlinebtn" onClick={signInWithGoogle}>
+          Sign in with Google
+        </button>
+      }
     </div>
   );
 }
