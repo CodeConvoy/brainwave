033ef2d6687727b70089678d75d9806a9345d1da
Cooper Saye <coopersaye@gmail.com>
2021-10-15 22:42:09 -0700

    Remove unused functions.

--- pages/ideas.js
+++ pages/ideas.js
@@ -11,49 +11,15 @@ import styles from '../styles/pages/Ideas.module.css';
 
 export default function Home() {
   const auth = getAuth();
-  const db = getFirestore();
-
-  const [title, setTitle] = useState('');
-
-  // get ideas query
-  const uid = auth.currentUser?.uid;
-  const ideasRef = collection(db, 'ideas');
-
-  // creates a new idea
-  async function createIdea() {
-    // create document in firebase
-    const now = new Date().getTime();
-    const docRef = await addDoc(ideasRef, {
-      title, creator: uid,
-      created: now,
-      modified: now,
-      sketch: null,
-      notes: []
-    });
-    // go to idea page
-    Router.push(`/ideas/${docRef.id}`);
-  }
 
   // return if loading
-  if (!uid) {
+  if (!auth.currentUser) {
     return <Loading />;
   }
 
   return (
     <div className={styles.container}>
       <Ideas />
-      <form onSubmit={e => {
-        e.preventDefault();
-        createIdea();
-      }}>
-        <input
-          placeholder="title"
-          value={title}
-          onChange={e => setTitle(e.target.value)}
-          required
-        />
-        <button>Create Idea</button>
-      </form>
       <Link href="/">
         <a className="link">Home</a>
       </Link>
