d1a22557d2f337cf4b18e8953318e5118fcaefa6
Cooper Saye <coopersaye@gmail.com>
2021-10-16 11:47:19 -0700

    Create header logo.

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,19 @@
+import Image from 'next/image';
+
 import styles from '../styles/components/Header.module.css';
 
 export default function Header() {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <Image
+          src="/logo.png"
+          width="48"
+          height="48"
+          alt="logo"
+        />
+      </div>
+      <h1>brainwave</h1>
     </div>
   );
 }
