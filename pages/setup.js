d86df622907e6d818cc94bf0e60508f2f0b1e190
Cooper Saye <coopersaye@gmail.com>
2021-10-15 22:41:34 -0700

    Implement user doc creation.

--- pages/setup.js
+++ pages/setup.js
@@ -1,13 +1,27 @@
 import { useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, doc, setDoc } from 'firebase/firestore';
 
 import styles from '../styles/pages/Setup.module.css';
 
 export default function Setup() {
   const [username, setUsername] = useState('');
 
+  const db = getFirestore();
+  const auth = getAuth();
+
+  const usersRef = collection(db, 'users');
+
+  // attempts to create user
+  async function createUser() {
+    const uid = auth.currentUser.uid;
+    setDoc(doc(usersRef, uid), { username });
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.center}>
+        <h1>New User</h1>
         <form onSubmit={e => {
           e.preventDefault();
           createUser();
