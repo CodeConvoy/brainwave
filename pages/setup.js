74d1d4c3b19c994f59e524b6b8e2bd778d219c36
Cooper Saye <coopersaye@gmail.com>
2021-10-15 22:28:55 -0700

    Create username form.

--- pages/setup.js
+++ pages/setup.js
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+
 import styles from '../styles/pages/Setup.module.css';
 
 export default function Setup() {
+  const [username, setUsername] = useState('');
+
   return (
     <div>
+      <form onSubmit={e => {
+        e.preventDefault();
+        createUser();
+      }}>
+        <input
+          placeholder="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          required
+        />
+        <button>Create User</button>
+      </form>
     </div>
   );
 }
