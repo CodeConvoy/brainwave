bf687346cc7caef5fcaf5543b3bd856e3355d706
Cooper Saye <coopersaye@gmail.com>
2021-10-16 10:44:50 -0700

    Listen for user data.

--- components/Main.js
+++ components/Main.js
@@ -1,5 +1,22 @@
 import { useEffect, useState } from 'react';
 import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, doc } from 'firebase/firestore';
+import { useDocumentData } from 'react-firebase9-hooks/firestore';
+
+function MainAuthed(props) {
+  const { Component, pageProps } = props;
+
+  const db = getFirestore();
+  const auth = getAuth();
+
+  // get user data
+  const uid = auth.currentUser.uid;
+  const usersRef = collection(db, 'users');
+  const userRef = doc(db, 'users', uid);
+  const [userData] = useDocumentData(userRef);
+
+  return <Component authed={true} userData={userData} {...pageProps} />;
+}
 
 export default function Main(props) {
   const { Component, pageProps } = props;
