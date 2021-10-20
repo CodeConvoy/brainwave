4cb0a163438a9a4052511d0faf8b775af17823b8
Cooper Saye <coopersaye@gmail.com>
2021-10-20 11:11:28 -0700

    Improve IdeaButton styling.

--- components/IconButton.js
+++ components/IconButton.js
@@ -1,8 +1,15 @@
 import styles from '../styles/components/IconButton.module.css';
 
 export default function IconButton(props) {
+  const { className, onClick } = props;
+
   return (
-    <button>
+    <button
+      className={
+        className ? `${styles.container} ${className}` : styles.container
+      }
+      onClick={onClick}
+    >
       {props.children}
     </button>
   );
