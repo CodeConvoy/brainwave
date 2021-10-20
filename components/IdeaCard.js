0d9094a799ace52b55eb23e2035e706130885843
Cooper Saye <coopersaye@gmail.com>
2021-10-19 21:39:39 -0700

    Create idea card modal.

--- components/IdeaCard.js
+++ components/IdeaCard.js
@@ -1,12 +1,17 @@
+import Modal from './Modal';
 import Router from 'next/router';
 import EditIcon from '@mui/icons-material/Edit';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 
+import { useState } from 'react';
+
 import styles from '../styles/components/IdeaCard.module.css';
 
 export default function IdeaCard(props) {
   const { title, id } = props;
+
+  const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <>
@@ -17,13 +22,19 @@ export default function IdeaCard(props) {
         <CardContent>
           <button
             className={styles.editbutton}
-            onClick={e => e.stopPropagation()}
+            onClick={e => {
+              e.stopPropagation();
+              setModalOpen(true);
+            }}
           >
             <EditIcon />
           </button>
           <p>{title}</p>
         </CardContent>
       </Card>
+      <Modal open={modalOpen} setOpen={setModalOpen}>
+        <h1>Editing {title}</h1>
+      </Modal>
     </>
   );
 }
