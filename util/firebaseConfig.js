d53632911f6b300f08ce78b9c215902262d1979e
Cooper Saye <coopersaye@gmail.com>
2021-10-19 19:55:06 -0700

    Prepare for deploy.

---
+++ util/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDzagFyzBlso2VBLnEdNvznFyJdQipKOhg",
+  authDomain: "brainwave-app.firebaseapp.com",
+  projectId: "brainwave-app",
+  storageBucket: "brainwave-app.appspot.com",
+  messagingSenderId: "796457479091",
+  appId: "1:796457479091:web:aaedc170d714a7c2bb39c6"
+};
