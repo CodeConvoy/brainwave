93588b985b4321524adf24475b76a751ce7d9590
Cooper Saye <coopersaye@gmail.com>
2021-10-29 21:54:34 -0700

    Implement canvas sketching.

--- components/Canvas.js
+++ components/Canvas.js
@@ -3,6 +3,11 @@ import { useEffect, useRef, useState } from 'react';
 const canvasWidth = 2048;
 const canvasHeight = 2048;
 let canvas, ctx;
+let sketching;
+
+let prevX, prevY;
+let currX, currY;
+
 
 export default function Canvas() {
   const [loading, setLoading] = useState(true);
@@ -13,6 +18,32 @@ export default function Canvas() {
     canvas = canvasRef.current;
     ctx = canvas.getContext('2d');
   }, []);
+
+  // gets previous and current mouse positions
+  function sketch(e) {
+    prevX = currX;
+    prevY = currY;
+    currX = e.clientX - canvas.offsetLeft + container.scrollLeft;
+    currY = e.clientY - canvas.offsetTop + container.scrollTop;
+  }
+
+  // ends sketching
+  function endSketch() {
+    if (!sketching) return;
+    sketching = false;
+    saveCanvas();
+  }
+
+  // draws on canvas with current sketch data
+  function draw(e) {
+    sketch(e);
+    // draw stroke
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.stroke();
+    ctx.closePath();
+  }
 
 
   return (
