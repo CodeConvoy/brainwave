e51b06281e2465c7a7d5689225289be5d5187638
Cooper Saye <coopersaye@gmail.com>
2021-10-29 21:54:04 -0700

    Get canvas context.

--- components/Canvas.js
+++ components/Canvas.js
@@ -1,23 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const canvasWidth = 2048;
 const canvasHeight = 2048;
+let canvas, ctx;
 
 export default function Canvas() {
   const [loading, setLoading] = useState(true);
+  const canvasRef = useRef();
+
+  // get canvas context on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
+
 
   return (
-    <div>
-      <canvas
-        style={loading ? { display: 'none' } : null}
-        ref={canvasRef}
-        width={canvasWidth}
-        height={canvasHeight}
-        onMouseDown={e => { sketching = true; sketch(e); }}
-        onMouseMove={e => { if (sketching) draw(e); }}
-        onMouseUp={endSketch}
-        onMouseLeave={endSketch}
-      />
-    </div>
+    <canvas
+      style={loading ? { display: 'none' } : null}
+      ref={canvasRef}
+      width={ideaData?.pixels}
+      height={ideaData?.pixels}
+      onMouseDown={e => { sketching = true; sketch(e); }}
+      onMouseMove={e => { if (sketching) draw(e); }}
+      onMouseUp={endSketch}
+      onMouseLeave={endSketch}
+    />
   );
 }
