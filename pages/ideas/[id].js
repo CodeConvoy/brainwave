a55671ee9cadce8c106a59adf3728f4a58cf2d4c
Cooper Saye <coopersaye@gmail.com>
2021-10-29 22:02:16 -0700

    Remove unused functions.

--- pages/ideas/[id].js
+++ pages/ideas/[id].js
@@ -64,37 +64,6 @@ export default function Idea() {
 
   const [minimapOpen, setMinimapOpen] = useState(true);
 
-  // retrieves idea from firebase
-  async function getIdea() {
-    // get idea data
-    const ideaRef = doc(db, 'ideas', id);
-    const ideaDoc = await getDoc(ideaRef);
-    setIdea({ ...ideaDoc.data(), id: ideaDoc.id });
-    // set idea data
-    const ideaData = ideaDoc.data();
-    setNotes(ideaData.notes);
-    const sketch = ideaData.sketch;
-    // if sketch, load image to canvas
-    if (sketch) {
-      const image = new Image();
-      image.onload = () => {
-        ctx.drawImage(image, 0, 0);
-        miniCtx.drawImage(image, 0, 0, miniWidth, miniHeight);
-        setLoading(false);
-      }
-      image.src = sketch;
-    // if no sketch, clear canvas
-    } else {
-      clearCanvas();
-      setLoading(false);
-    }
-  }
-
-  // get idea on start
-  useEffect(() => {
-    if (id && uid) getIdea();
-  }, [id, uid]);
-
   // scrolls container by given values
   function scrollContainer(x, y) {
     if (x) container.scrollLeft += x;
@@ -110,8 +79,6 @@ export default function Idea() {
   // on start
   useEffect(() => {
     // get element references
-    canvas = canvasRef.current;
-    ctx = canvas.getContext('2d');
     miniCanvas = miniCanvasRef.current;
     miniCtx = miniCanvas.getContext('2d');
     container = containerRef.current;
@@ -119,69 +86,6 @@ export default function Idea() {
     container.addEventListener('wheel', onWheel);
     return () => container.removeEventListener('wheel', onWheel);
   }, []);
-
-  // gets previous and current mouse positions
-  function sketch(e) {
-    prevX = currX;
-    prevY = currY;
-    currX = e.clientX - canvas.offsetLeft + container.scrollLeft;
-    currY = e.clientY - canvas.offsetTop + container.scrollTop;
-  }
-
-  // ends sketching
-  function endSketch() {
-    if (!sketching) return;
-    sketching = false;
-    saveCanvas();
-  }
-
-  // draws on canvas with current sketch data
-  function draw(e) {
-    sketch(e);
-    // draw stroke
-    ctx.beginPath();
-    ctx.moveTo(prevX, prevY);
-    ctx.lineTo(currX, currY);
-    ctx.stroke();
-    ctx.closePath();
-  }
-
-  // fills canvas with white
-  function clearCanvas() {
-    ctx.fillStyle = 'white';
-    ctx.rect(0, 0, canvasWidth, canvasHeight);
-    ctx.fill();
-    saveCanvas();
-    notesDirty = true;
-    setNotes([]);
-  }
-
-  // downloads canvas as a png
-  function downloadCanvas() {
-    // get canvas object url
-    canvas.toBlob(blob => {
-      const url = URL.createObjectURL(blob);
-      // download from link element
-      const link = document.createElement('a');
-      link.download = 'idea.png';
-      link.href = url;
-      link.click();
-    });
-  }
-
-  // saves canvas as data url
-  async function saveCanvas() {
-    // save to firebase
-    const sketch = canvas.toDataURL();
-    const ideaRef = doc(db, 'ideas', id);
-    await updateDoc(ideaRef, { sketch });
-    // draw minimap
-    const image = new Image();
-    image.onload = () => {
-      miniCtx.drawImage(image, 0, 0, miniWidth, miniHeight);
-    }
-    image.src = sketch;
-  }
 
   // creates a blank note on canvas
   function createNote() {
