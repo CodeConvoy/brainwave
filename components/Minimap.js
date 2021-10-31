dbe4b02574da0f8c4d3be775aeedd6816e503d89
Cooper Saye <coopersaye@gmail.com>
2021-10-30 21:07:49 -0700

    Draw minimap on idea change.

--- components/Minimap.js
+++ components/Minimap.js
@@ -3,10 +3,32 @@ import { useRef, useState } from 'react';
 const width = 256;
 const height = 256;
 
-export default function Minimap() {
+export default function Minimap(props) {
+  const { ideaData } = props;
+
   const canvasRef = useRef();
 
   const [open, setOpen] = useState(false);
+
+  // update canvas when idea data changes
+  useEffect(() => {
+    if (ideaData) {
+      const sketch = ideaData.sketch;
+      // if sketch, load image to canvas
+      if (sketch) {
+        const image = new Image();
+        image.onload = () => {
+          ctx.drawImage(image, 0, 0);
+          setLoading(false);
+        }
+        image.src = sketch;
+      // if no sketch, clear canvas
+      } else {
+        clearCanvas();
+        setLoading(false);
+      }
+    }
+  }, [ideaData]);
 
   return (
     <div>
