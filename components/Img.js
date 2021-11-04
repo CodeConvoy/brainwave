6480a258c413c4ec999691adbe76d4465a4f9ce0
Cooper Saye <coopersaye@gmail.com>
2021-11-03 21:33:04 -0700

    Fix image styling.

--- components/Img.js
+++ components/Img.js
@@ -1,8 +1,11 @@
+import styles from '../styles/components/Img.module.css';
+
 export default function Img(props) {
   const { url, x, y } = props;
 
   return (
     <img
+      className={styles.container}
       src={url}
       style={{ left: x, top: y }}
     />
