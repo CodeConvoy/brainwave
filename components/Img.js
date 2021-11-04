16594063e278af928ed1b065f79ed8e0da1bcb33
Cooper Saye <coopersaye@gmail.com>
2021-11-03 21:40:31 -0700

    Improve image layout.

--- components/Img.js
+++ components/Img.js
@@ -1,13 +1,19 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+
 import styles from '../styles/components/Img.module.css';
 
 export default function Img(props) {
   const { url, x, y } = props;
 
   return (
-    <img
+    <div
       className={styles.container}
-      src={url}
       style={{ left: x, top: y }}
-    />
+    >
+      <img src={url} />
+      <button onClick={()=>{}}>
+        <DeleteIcon />
+      </button>
+    </div>
   );
 }
