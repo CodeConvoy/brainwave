57f2d106aba1f7e3d824dc4a636e3d63cf92a811
Cooper Saye <coopersaye@gmail.com>
2021-11-04 23:25:50 -0700

    Update site meta.

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,9 @@ import { firebaseConfig } from '../util/firebaseConfig';
 
 import '../styles/globals.css';
 
+// meta
 const title = "Brainwave";
+const url = "https://brainwave.vercel.app";
 const description = "Rich and centralized idea development.";
 const image = "https://brainwave.vercel.app/img/cover.png";
 
@@ -17,6 +19,7 @@ export default function App(props) {
   return (
     <>
       <Head>
+        {/* general */}
         <title>{title}</title>
         <meta name="description" content={description} />
         {/* links */}
@@ -25,16 +28,16 @@ export default function App(props) {
         <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;900&display=swap" rel="stylesheet" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="canonical" href="https://brainwave.vercel.app" />
+        <link rel="canonical" href={url} />
         {/* open graph */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://brainwave.vercel.app" />
+        <meta property="og:url" content={url} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
         {/* twitter */}
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@csaye_" />
+        <meta name="twitter:site" content="@CodeConvoyOrg" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={image} />
